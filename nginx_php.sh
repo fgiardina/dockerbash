3e567c66fa9bccb22ec3e0ba5caf39031e8e7cd7
Fernando Giardina <fgiardina@beatmobile.com.ar>
2021-09-07 13:38:27 -0300

    full

--- nginx_php.sh
+++ nginx_php.sh
@@ -1,7 +1,12 @@
 #!bin/bash
 
+ENV DEBIAN_FRONTEND noninteractive
+ENV TZ=UTC
+
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
 sudo apt-get update \
-    && apt-get install -y gnupg gosu curl ca-certificates zip unzip git supervisor sqlite3 libcap2-bin libpng-dev python2 \
+    && apt-get install -y gnupg gosu curl ca-certificates zip unzip git supervisor sqlite3 libcap2-bin libpng-dev python3 \
     && mkdir -p ~/.gnupg \
     && chmod 600 ~/.gnupg \
     && echo "disable-ipv6" >> ~/.gnupg/dirmngr.conf \
@@ -10,12 +15,12 @@ sudo apt-get update \
     && echo "deb http://ppa.launchpad.net/ondrej/php/ubuntu focal main" > /etc/apt/sources.list.d/ppa_ondrej_php.list \
     && apt-get update \
     && apt-get install -y php8.0-cli php8.0-dev \
-       php8.0-gd \
-       php8.0-curl \
+       php8.0-pgsql php8.0-sqlite3 php8.0-gd \
+       php8.0-curl php8.0-memcached \
        php8.0-imap php8.0-mysql php8.0-mbstring \
        php8.0-xml php8.0-zip php8.0-bcmath php8.0-soap \
        php8.0-intl php8.0-readline \
-       php8.0-msgpack php8.0-igbinary \
+       php8.0-msgpack php8.0-igbinary php8.0-ldap \
        php8.0-redis \
     && php -r "readfile('http://getcomposer.org/installer');" | php -- --install-dir=/usr/bin/ --filename=composer \
     && curl -sL https://deb.nodesource.com/setup_16.x | bash - \
@@ -25,7 +30,14 @@ sudo apt-get update \
     && apt-get update \
     && apt-get install -y yarn \
     && apt-get install -y mysql-client \
-    && apt-get install nginx -y 
+    && apt-get install -y postgresql-client \
     && apt-get -y autoremove \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+sudo pecl channel-update https://pecl.php.net/channel.xml \
+    && pecl install swoole \
+    && pecl clear-cache \
+    && rm -rf /tmp/* /var/tmp/*
+
+sudo setcap "cap_net_bind_service=+ep" /usr/bin/php8.0
