16d61e62cafeacc85873c6693875255c7aadd24b
fgiardina <fgiardina@outlook.com>
2022-08-03 13:08:42 -0300

    update

--- nginx_php.sh
+++ nginx_php.sh
@@ -10,6 +10,8 @@ sudo apt-get update \
     && apt-key adv --homedir ~/.gnupg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E5267A6C \
     && apt-key adv --homedir ~/.gnupg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C300EE8C \
     && echo "deb http://ppa.launchpad.net/ondrej/php/ubuntu focal main" > /etc/apt/sources.list.d/ppa_ondrej_php.list \
+    && apt-get install -y ca-certificates apt-transport-https software-properties-common \
+    && add-apt-repository ppa:ondrej/php
     && apt-get update \
     && apt-get install -y php8.0-cli php8.0-dev php8.0-fpm \
        php8.0-gd \
