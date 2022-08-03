b3fe6c9d3917b1155715f47351b0c4df1dd208ae
fgiardina <fgiardina@outlook.com>
2022-08-03 12:59:07 -0300

    update

--- nginx_php.sh
+++ nginx_php.sh
@@ -3,7 +3,7 @@
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo UTC > /etc/timezone
 
 sudo apt-get update \
-    && apt-get install -y gnupg gosu curl ca-certificates zip unzip git supervisor sqlite3 libcap2-bin libpng-dev python3 \
+    && apt-get install -y gnupg gosu curl ca-certificates zip unzip git supervisor libcap2-bin libpng-dev python3 \
     && mkdir -p ~/.gnupg \
     && chmod 600 ~/.gnupg \
     && echo "disable-ipv6" >> ~/.gnupg/dirmngr.conf \
@@ -11,8 +11,8 @@ sudo apt-get update \
     && apt-key adv --homedir ~/.gnupg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C300EE8C \
     && echo "deb http://ppa.launchpad.net/ondrej/php/ubuntu focal main" > /etc/apt/sources.list.d/ppa_ondrej_php.list \
     && apt-get update \
-    && apt-get install -y php8.0-cli php8.0-dev \
-       php8.0-pgsql php8.0-sqlite3 php8.0-gd \
+    && apt-get install -y php8.0-cli php8.0-dev php8.0-fpm \
+       php8.0-gd \
        php8.0-curl php8.0-memcached \
        php8.0-imap php8.0-mysql php8.0-mbstring \
        php8.0-xml php8.0-zip php8.0-bcmath php8.0-soap \
@@ -26,16 +26,8 @@ sudo apt-get update \
     && echo "deb https://dl.yarnpkg.com/debian/ stable main" > /etc/apt/sources.list.d/yarn.list \
     && apt-get update \
     && apt-get install -y yarn \
-    && apt-get install -y mysql-client \
-    && apt-get install -y postgresql-client \
+    && apt-get install -y mysql-server \
     && apt-get install -y nginx \
     && apt-get -y autoremove \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-# pecl channel-update https://pecl.php.net/channel.xml \
-#     && pecl install swoole \
-#     && pecl clear-cache \
-#     && rm -rf /tmp/* /var/tmp/*
-
-# setcap "cap_net_bind_service=+ep" /usr/bin/php8.0
