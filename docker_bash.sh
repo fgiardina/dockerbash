02826d5a297f3ce3ca40b971a2d8e3e735a67cff
fgiardina <fgiardina@outlook.com>
2023-04-22 14:01:53 -0300

    2.17.3 compose
    2.17.3 compose

--- docker_bash.sh
+++ docker_bash.sh
@@ -7,7 +7,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 sudo apt-get install docker-ce -y
 docker --version
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/2.17.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 sudo apt-get install zip unzip
